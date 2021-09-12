55f0e179638b990f21e51be1c5e9dd64b712a392
MkDay <89559065+MkDay@users.noreply.github.com>
2021-09-13 00:59:04 +0530

    Create hex.js

---
+++ hex.js
@@ -0,0 +1,20 @@
+var arr = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f"];
+
+function randomHexColors(element) {  
+  var r = bg();
+  document.getElementById(element).style.background = r;
+  
+  document.getElementById(element).innerHTML = r; //opional
+  // instead use console to get the hex color value
+  console.log(r);
+}
+
+function bg() {
+  var hex = "#";
+  
+  for(var i=0; i<6; i++) {
+  hex += arr[Math.floor(Math.random()* arr.length)]; 
+  }
+  
+  return hex;
+}
