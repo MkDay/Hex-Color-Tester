16df1edfdadc711ad1a11451c616eaa639de45f5
MkDay <89559065+MkDay@users.noreply.github.com>
2021-09-17 01:14:36 +0530

    Update hex.js

--- hex.js
+++ hex.js
@@ -1,7 +1,7 @@
 var arr = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f"];
 
 function randomHexColors(element) {  
-  var r = bg();
+  var r = hexCode();
   document.getElementById(element).style.background = r;
   
   document.getElementById(element).innerHTML = r; //opional
@@ -9,7 +9,7 @@ function randomHexColors(element) {
   console.log(r);
 }
 
-function bg() {
+function hexCode() {
   var hex = "#";
   
   for(var i=0; i<6; i++) {
